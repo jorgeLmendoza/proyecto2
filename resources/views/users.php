93bbd7b7652267bbc53e3e7930a3e52c1da08415
jorgeLmendoza <jorge_mendoza@hotmail.com>
2020-03-08 15:38:22 -0600

    version 1

---
+++ resources/views/users.php
@@ -0,0 +1,20 @@
+<!doctype html>
+
+<html long="en">
+
+<head>
+    <meta xharset="UTF-8">
+    <meta name="viewport"
+            content="width=device-width, user-scalable=no, initial-scale=1.0, aximun-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Listado de usuarios - Styde.net</title>
+    </head>
+    <body>
+        <h1>Usuarios</h1>
+        
+        <ul>
+            <li> jorge Enrique Leon Mendoza </li>
+        </ul>
+
+    </body>
+    </html>
